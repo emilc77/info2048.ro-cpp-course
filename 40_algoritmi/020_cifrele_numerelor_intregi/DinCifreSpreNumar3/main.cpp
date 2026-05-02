f1a95f21d98a3183718449740be4bf2c4b40125c
emil <emilc77@gmail.com>
2026-05-02 17:29:10 +0300

    algoritmii legati de cifre

---
+++ 40_algoritmi/020_cifrele_numerelor_intregi/DinCifreSpreNumar3/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int cifre [] = {1,2,3,4};
+    int lungime_arr = 4;
+
+    int numar = 0;
+    for (int i = 0; i < lungime_arr; ++i) {
+        if (i != 0) {
+            numar *= 10;
+        }
+        numar += cifre[i];
+    }
+    cout << numar << endl; // 1234
+
+    return 0;
+}
