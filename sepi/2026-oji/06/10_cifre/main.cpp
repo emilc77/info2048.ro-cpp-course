42d7b9e1f5f7b25d2ec5b6bb7ecb15d48d14cecf
emil <emilc77@gmail.com>
2026-06-20 22:04:34 +0300

    refactor: improve comments and control flow in date validation logic

--- sepi/2026-oji/06/10_cifre/main.cpp
+++ sepi/2026-oji/06/10_cifre/main.cpp
@@ -73,7 +73,7 @@ bool uniq_digits(int year2, int month = -1, int day = -1,
         digits.push_back(minute % 10);
     }
 
-    int possible_digits [10] {}; // initializat cu 0
+    int possible_digits [10] {}; // initializat cu {0,0,0,0,0,....}
     for (int digit : digits) {
         if (possible_digits[digit] == 0) {
             possible_digits[digit] = 1;
@@ -90,14 +90,14 @@ int cerinta_2(int year4) {
     // zzllaahhmm
     bool bisect = year4 % 400 == 0 || (year4 % 4 == 0 && year4 % 100 != 0);
 
-    cout << uniq_digits(10,23,45,67,89) << endl;
+    // cout << uniq_digits(10,23,45,67,89) << endl;
 
     int counter = 0;
     int year2 = year4 % 100;
     if (uniq_digits(year2)) {
         for (int month = 1; month <= 12; ++month) {
             if (!uniq_digits(year2, month)) {
-                break;
+                continue;
             }
             int days_in_month = 31;
             if (month == 4 || month == 6 || month == 9 || month == 11) {
@@ -112,12 +112,12 @@ int cerinta_2(int year4) {
 
             for (int day = 1; day <= days_in_month; ++day) {
                 if (!uniq_digits(year2, month, day)) {
-                    break;
+                    continue;
                 }
 
                 for (int hour = 0; hour <= 23; ++hour) {
                     if (!uniq_digits(year2, month, day, hour)) {
-                        break;
+                        continue;
                     }
 
                     for (int minute = 0; minute <= 59; ++minute) {
